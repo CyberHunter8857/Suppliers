f7b70858d63fb67a2b283d41f66caee0637361be
Mayur Tamanke <136753085+CyberHunter8857@users.noreply.github.com>
2025-04-23 17:19:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ collection = db['submissions']
 
 @app.route('/')
 def index():
-    return render_template('index1.html')
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
