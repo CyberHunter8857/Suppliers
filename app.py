646428a9b4c3acee70e995de785027628a389340
Mayur Tamanke <136753085+CyberHunter8857@users.noreply.github.com>
2025-04-23 17:17:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,42 @@
-from flask import Flask, render_template, request, send_file
-from docx import Document
-import io
+from flask import Flask, render_template, request, jsonify, send_file
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Function to replace placeholders with student details in the docx file
-def replace_placeholders_in_docx(name, class_name, roll_number):
-    # Load the existing assignment.docx
-    doc = Document("assignment.docx")
-    
-    # Replace placeholders with the provided values
-    for paragraph in doc.paragraphs:
-        if 'xyz' in paragraph.text:
-            paragraph.text = paragraph.text.replace('xyz', name)
-        if 'classssss' in paragraph.text:
-            paragraph.text = paragraph.text.replace('classssss', class_name)
-        if '11111111' in paragraph.text:
-            paragraph.text = paragraph.text.replace('11111111', roll_number)
-
-    # Save the modified document to a BytesIO object for download
-    doc_io = io.BytesIO()
-    doc.save(doc_io)
-    doc_io.seek(0)
-    
-    return doc_io
+# MongoDB Connection
+client = MongoClient("mongodb+srv://admin:mayur123@cyberhunter.h1bcf.mongodb.net/?retryWrites=true&w=majority&appName=CyberHunter")
+db = client['java_lab']
+collection = db['submissions']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
     student_name = request.form['student_name']
     class_name = request.form['class_name']
     roll_number = request.form['roll_number']
-    
-    # Generate the modified docx file with the provided details
-    updated_docx = replace_placeholders_in_docx(student_name, class_name, roll_number)
-    
-    # Send the modified document as a download
-    return send_file(updated_docx, as_attachment=True, download_name=f"assignment_{student_name}.docx", mimetype="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+    submission = {
+        'name': student_name,
+        'class': class_name,
+        'roll_number': roll_number,
+        'timestamp': datetime.utcnow()
+    }
+    collection.insert_one(submission)
+
+    # Respond with success
+    return jsonify({"success": True})
+
+@app.route('/download_zip1')
+def download_zip1():
+    return send_file("Advance_Java_Lab_Manual.rar", as_attachment=True)
+
+@app.route('/download_zip2')
+def download_zip2():
+    return send_file("AJP_Final_Codes.rar", as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
